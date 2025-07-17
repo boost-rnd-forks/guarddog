a4fe4ab4fbbd16b5470a6740827ef1492c3ec856
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-17 18:11:02 -0400

    resolving conflicts

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -1,7 +1,8 @@
 # Maven-specific metadata security rules
 # Currently empty - rules can be added here in the future
+from guarddog.analyzer.metadata.detector import Detector
 
-MAVEN_METADATA_RULES = {} 
+MAVEN_METADATA_RULES: dict[str, Detector] = {}
 
 classes = [
 ]
