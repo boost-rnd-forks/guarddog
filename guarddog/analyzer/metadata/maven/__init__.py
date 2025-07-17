007c5dc00cd0112d86b21934a9383edd058eeeb6
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-17 15:38:50 -0400

    adding python scripts scanning handling in sourcecode

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -2,3 +2,10 @@
 # Currently empty - rules can be added here in the future
 
 MAVEN_METADATA_RULES = {} 
+
+classes = [
+]
+
+for detectorClass in classes:
+    detectorInstance = detectorClass()  # type: ignore
+    MAVEN_METADATA_RULES[detectorInstance.get_name()] = detectorInstance
