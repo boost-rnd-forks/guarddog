c7e6af376abf699a738619647b4bfcbcc5bdc4d5
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-21 15:45:02 -0400

    formatting

--- guarddog/analyzer/metadata/maven/unclaimed_maintainer_email_domain.py
+++ guarddog/analyzer/metadata/maven/unclaimed_maintainer_email_domain.py
@@ -9,5 +9,5 @@ class MavenUnclaimedMaintainerEmailDomainDetector(
     def __init__(self):
         super().__init__("maven")
 
-    def get_email_addresses(self, package_info: dict)-> set[str]:
-        return set(package_info['info']["email"])
+    def get_email_addresses(self, package_info: dict) -> set[str]:
+        return set(package_info["info"]["email"])
