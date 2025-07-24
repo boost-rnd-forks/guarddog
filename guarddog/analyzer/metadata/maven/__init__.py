0aad9e2b86971bc6d863f20e753bd9c2f00306f8
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-24 12:32:44 -0400

    call bundle binray geenric method on decompressed file

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -2,5 +2,14 @@
 # Currently empty - rules can be added here in the future
 
 from guarddog.analyzer.metadata.detector import Detector
+from guarddog.analyzer.metadata.maven.bundled_binary import MavenBundledBinary
 
 MAVEN_METADATA_RULES: dict[str, Detector] = {}
+
+classes = [
+    MavenBundledBinary,
+]
+
+for detectorClass in classes:
+    detectorInstance = detectorClass()  # type: ignore
+    MAVEN_METADATA_RULES[detectorInstance.get_name()] = detectorInstance
