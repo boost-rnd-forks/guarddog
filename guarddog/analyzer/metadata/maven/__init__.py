eb4f73ce2d668050dc145d383439549976f5db24
Saif <saif@saifs-air.home>
2025-07-25 13:08:33 -0400

    Added deceptive author with maven logic

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -1,6 +1,13 @@
-# Maven-specific metadata security rules
-# Currently empty - rules can be added here in the future
 
 from guarddog.analyzer.metadata.detector import Detector
+from guarddog.analyzer.metadata.maven.deceptive_author import MavenDeceptiveAuthor
 
 MAVEN_METADATA_RULES: dict[str, Detector] = {}
+
+classes = [
+    MavenDeceptiveAuthor,
+]
+
+for detectorClass in classes:
+    detectorInstance = detectorClass()  # type: ignore
+    MAVEN_METADATA_RULES[detectorInstance.get_name()] = detectorInstance
---
+++ guarddog/analyzer/metadata/maven/utils.py
@@ -0,0 +1,26 @@
+def get_email_addresses(package_info: dict) -> set[str]:
+    """
+    Extract email addresses from Maven package metadata.
+    
+    Args:
+        package_info (dict): Maven package metadata
+        
+    Returns:
+        set[str]: Set of email addresses found in the metadata
+    """
+    emails = set()
+    
+    # Extract from developers
+    developers = package_info.get("developers", [])
+    for dev in developers:
+        if "email" in dev:
+            emails.add(dev["email"])
+            
+    # Extract from contributors
+    contributors = package_info.get("contributors", [])
+    for contrib in contributors:
+        if "email" in contrib:
+            emails.add(contrib["email"])
+            
+    return emails - {None, ""}
+ 
