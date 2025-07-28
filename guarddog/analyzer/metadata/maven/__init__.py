f44a865c23879392e0b5a3775a9bf541de3b3796
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-28 11:08:08 -0400

    empty <description> field in remote pom.xml detector

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -2,5 +2,14 @@
 # Currently empty - rules can be added here in the future
 
 from guarddog.analyzer.metadata.detector import Detector
+from guarddog.analyzer.metadata.maven.empty_information import MavenEmptyInfoDetector
 
 MAVEN_METADATA_RULES: dict[str, Detector] = {}
+
+classes = [
+    MavenEmptyInfoDetector,
+]
+
+for detectorClass in classes:
+    detectorInstance = detectorClass()  # type: ignore
+    MAVEN_METADATA_RULES[detectorInstance.get_name()] = detectorInstance
