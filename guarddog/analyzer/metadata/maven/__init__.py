6914d61b6695214545a3149e0da3196776953b07
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-28 11:16:10 -0400

    adding metadata rule in list to run

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -2,5 +2,16 @@
 # Currently empty - rules can be added here in the future
 
 from guarddog.analyzer.metadata.detector import Detector
+from guarddog.analyzer.metadata.maven.potentially_compromised_email_domain import (
+    MavenPotentiallyCompromisedEmailDomainDetector,
+)
 
 MAVEN_METADATA_RULES: dict[str, Detector] = {}
+
+classes = [
+    MavenPotentiallyCompromisedEmailDomainDetector,
+]
+
+for detectorClass in classes:
+    detectorInstance = detectorClass()  # type: ignore
+    MAVEN_METADATA_RULES[detectorInstance.get_name()] = detectorInstance
