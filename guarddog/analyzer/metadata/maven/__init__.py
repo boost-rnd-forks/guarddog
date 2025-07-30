a61c38a5c5f217546cdd4a19cc919690d7572d67
Saif <saif@saifs-air.home>
2025-07-30 12:34:25 -0400

    Added script for repo integrity

--- guarddog/analyzer/metadata/maven/__init__.py
+++ guarddog/analyzer/metadata/maven/__init__.py
@@ -1,11 +1,13 @@
 # Maven-specific metadata security rules
-# Currently empty - rules can be added here in the future
 from typing import Type
 from guarddog.analyzer.metadata.detector import Detector
+from .repository_integrity_mismatch import MavenIntegrityMismatchDetector
 
 MAVEN_METADATA_RULES: dict[str, Detector] = {}
 
-classes: list[Type[Detector]] = []
+classes: list[Type[Detector]] = [
+    MavenIntegrityMismatchDetector,
+]
 
 for detectorClass in classes:
     detectorInstance = detectorClass()  # type: ignore
