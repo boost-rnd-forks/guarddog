ad3baa11b99bd2c0155ade652f5733845cd23ce7
Garance <garance@MacBook-Air-de-Garance.local>
2025-07-31 18:14:19 +0200

    checking package_info mail content

--- guarddog/analyzer/metadata/maven/unclaimed_maintainer_email_domain.py
+++ guarddog/analyzer/metadata/maven/unclaimed_maintainer_email_domain.py
@@ -10,4 +10,8 @@ class MavenUnclaimedMaintainerEmailDomainDetector(
         super().__init__("maven")
 
     def get_email_addresses(self, package_info: dict) -> set[str]:
-        return set(package_info["info"]["email"])
+        info = package_info.get("info", {})
+        emails = info.get("email", [])
+        if not isinstance(emails, (list, tuple, set)):
+            return set()
+        return set(emails)
