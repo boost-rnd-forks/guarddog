b8c09496399639faf694113503734006e7d3d2ef
Garance <garance@MacBook-Air-de-Garance.local>
2025-08-07 08:17:21 +0200

    cleaner way to get dict value

--- guarddog/analyzer/metadata/maven/empty_information.py
+++ guarddog/analyzer/metadata/maven/empty_information.py
@@ -1,7 +1,8 @@
-""" Empty Information Detector
+"""Empty Information Detector
 
 Detects if a package contains an empty description
 """
+
 import logging
 from typing import Optional
 
@@ -11,7 +12,17 @@ log = logging.getLogger("guarddog")
 
 
 class MavenEmptyInfoDetector(EmptyInfoDetector):
-    def detect(self, package_info, path: Optional[str] = None, name: Optional[str] = None,
-               version: Optional[str] = None) -> tuple[bool, str]:
-        log.debug(f"Running Maven empty description heuristic on package {name} version {version}")
-        return len(package_info["info"]["description"].strip()) == 0, EmptyInfoDetector.MESSAGE_TEMPLATE % "Maven"
+    def detect(
+        self,
+        package_info,
+        path: Optional[str] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+    ) -> tuple[bool, str]:
+        log.debug(
+            f"Running Maven empty description heuristic on package {name} version {version}"
+        )
+        return (
+            len(package_info.get("info").get("description").strip()) == 0,
+            EmptyInfoDetector.MESSAGE_TEMPLATE % "Maven",
+        )
