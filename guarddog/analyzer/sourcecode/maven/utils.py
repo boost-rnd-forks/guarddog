01b272d53f26ac46e2c795b0549e1a312463f6c4
Garance <garance@MacBook-Air-de-Garance.local>
2025-08-08 12:53:19 +0200

    metadata script unmaintained email in sourcecode for local projects analysis

---
+++ guarddog/analyzer/sourcecode/maven/utils.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+import logging
+
+from guarddog.utils.archives import find_pom
+
+NAMESPACE = {"mvn": "http://maven.apache.org/POM/4.0.0"}
+log = logging.getLogger("guarddog")
+
+
+def get_email_addresses(path: str) -> set[str]:
+    """
+    Extract email addresses from Maven package metadata.
+    Args:
+        package_info (dict): Maven package metadata
+    Returns:
+        set[str]: Set of email addresses found in the metadata
+    """
+    log.debug("looking for pom to find developer's emails...")
+    pom_path = find_pom(path)
+    if not pom_path:
+        log.error(f"Could not find pom.xml in {path}")
+        return set()
+    emails: list[str] = []
+    # find email
+    tree = ET.parse(pom_path)
+    root = tree.getroot()
+    emails = []
+    for dev in root.findall(".//mvn:developer", NAMESPACE):
+        email = dev.find("mvn:email", NAMESPACE)
+        if email is not None and email.text:
+            emails.append(email.text)
+    if not emails:
+        log.warning("No email found in the pom.")
+    return set(emails)
